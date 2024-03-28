52d7ec6a7514abc99f607f2de3ad11b5caeae90e
Sheldon <lhanco@gmail.com>
2024-03-28 12:33:25 +0700

    Day 46: Populate Table with data from SQLite.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -17,21 +18,35 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
-        # Add the below code if the Help menu was not appear (especially in Mac OS)
+        # Add the below code if the Help menu was not appear (especially in macOS)
         # about_action.setMenuRole(QAction.MenuRole.NoRole)
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        # Hide the index column of the table
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        pass
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+
+        # Reset the table
+        self.table.setRowCount(0)
+
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+        connection.close()
 
 
 app = QApplication(sys.argv)
 student_mgt_app = MainWindow()
 student_mgt_app.show()
+student_mgt_app.load_data()
 sys.exit(app.exec())
 
 
